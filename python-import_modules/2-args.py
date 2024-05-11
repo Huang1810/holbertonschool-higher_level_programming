aab500ef05a251a01c4a837aac64ec1839351ef4
Huang1810 <ahhamed1810@gmail.com>
2024-05-11 22:08:03 +0100

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("{} argument{}:".format(argc, 's' if argc != 1 else ''))
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
-            
+  
