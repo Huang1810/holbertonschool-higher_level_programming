668eed162c05ad3c4abd193e76fd93c7db30e372
Huang1810 <ahhamed1810@gmail.com>
2024-05-25 18:21:43 +0100

    commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-"""
-This module provides a function `add_integer` to add two integers.
-"""
+add_integer = __import__('0-add_integer').add_integer
 
-
-def add_integer(a, b=98):
-    """
-    Adds two integers or floats after casting them to integers.
-
-    Args:
-        a : must be an integer or float.
-        b : must be an integer or float, default is 98.
-
-    Returns:
-        int: The sum of a and b as an integer.
-
-    Raises:
-        TypeError: If either a or b is not an integer or float.
-    """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testfile("tests/0-add_integer.txt")
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
