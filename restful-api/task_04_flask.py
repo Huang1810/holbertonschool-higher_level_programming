cb05bb59253c664559df0039262280a14ea47531
Huang1810 <159008826+Huang1810@users.noreply.github.com>
2024-06-01 19:23:22 +0100

    Update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -25,16 +25,18 @@ def get_user(username):
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": "User not found"})
+        return jsonify({"error": "User not found"}), 404
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.json
-    username = data.get('username')
+    if 'username' not in data:
+        return jsonify({"error": "Username is required"}), 400
+    username = data['username']
     if username in users:
-        return jsonify({"error": "User already exists"})
+        return jsonify({"error": "User already exists"}), 400
     users[username] = data
-    return jsonify({"message": "User added", "user": data})
+    return jsonify({"message": "User added", "user": data}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
