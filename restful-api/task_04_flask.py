0f79ff3be658c5e8185447aaea7be1f5bce1477d
Huang1810 <159008826+Huang1810@users.noreply.github.com>
2024-06-01 21:10:03 +0100

    Update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,15 +7,19 @@ users = {
     "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
 }
 
+@app.route('/')
 def home():
     return "Welcome to the Flask API!"
 
+@app.route('/data')
 def get_data():
     return jsonify(list(users.keys()))
 
+@app.route('/status')
 def status():
     return "OK"
 
+@app.route('/users/<username>')
 def get_user(username):
     user = users.get(username)
     if user:
@@ -23,6 +27,7 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
+@app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.json
     if 'username' not in data:
