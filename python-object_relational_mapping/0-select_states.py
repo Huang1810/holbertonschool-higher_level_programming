7725d40c9a48c922a194e0a0b2b327b7aaf3be26
Huang1810 <159008826+Huang1810@users.noreply.github.com>
2024-06-22 23:26:11 +0100

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,17 +11,37 @@ Arguments:
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        return
+
     mySQL_u = sys.argv[1]
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
 
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
-    cur = db.cursor()
+    try:
+        # Establish a connection to the database
+        db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+        cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetchall()
+        # Execute the query to fetch all states ordered by id
+        cur.execute("SELECT * FROM states ORDER BY id")
+        rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+        # Print each row
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error {}: {}".format(e.args[0], e.args[1]))
+
+    finally:
+        # Close cursor and connection
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+if __name__ == "__main__":
+    main()
